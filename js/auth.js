1d392aeaf36bbfa2e258041a684238725687aced
M. Kandhasamy <kandhasamym77@gmail.com>
2026-07-04 12:48:37 +0530

    Initial commit - Student Attendance Management System

---
+++ js/auth.js
@@ -0,0 +1,8 @@
+<button onclick="window.location.href='dashboard.html'">
+    Login
+</button>
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+    window.location.href = "dashboard.html";
+});
